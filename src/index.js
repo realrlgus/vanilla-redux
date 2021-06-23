1ffaac5fb049c0c479cfcb09f5d7acf0233e3965
realrlgus <realrlgus@naver.com>
2021-06-23 22:01:50 +0900

    finish react-redux

--- src/index.js
+++ src/index.js
@@ -1,35 +1,14 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import store from "./store";
 
-const plus = document.getElementById("plus");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+
+ReactDOM.render(
+<Provider store={store}>
+  <App/>
+</Provider>
 
 
-const countModifier = (count = 0 , action) => {
-  if(action.type === "ADD"){
-    return count = count + 1;
-  }else if(action.type === "MINUS"){
-    return count = count - 1;
-  }
-  return count;
-};
-
-const onChange = () => {
-  number.innerText = countStore.getState();
-}
-
-const countStore = createStore(countModifier);
-
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({type : 'ADD'});
-}
-
-const handleMinus = () => {
-  countStore.dispatch({type : 'MINUS'});
-}
-
-plus.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-
+ , document.getElementById("root"));
